e09965bcd51381fc100d1fa50b45949f33de04a3
Najah7 <najahmatsuo7@gmail.com>
2023-12-27 03:16:10 +0900

    ✨ [Add] health-check endpoint and go.mod

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandlerFunc("/health-check", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "I am alive!")
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+}
